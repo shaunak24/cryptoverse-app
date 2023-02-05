acde64a93d9d000084a7aaf0202b61726c5ea1c1
Shaunak <shaunak241997@gmail.com>
2023-02-05 22:46:22 +0530

    Create app layout and initial routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { Route, Link, Routes } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import {
+  Navbar,
+  Cryptocurrencies,
+  Exchanges,
+  CryptoDetails,
+  News,
+  Homepage,
+} from './components';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <h1>Cryptoverse</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: 'white', textAlign: 'center' }}
+          >
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 };
