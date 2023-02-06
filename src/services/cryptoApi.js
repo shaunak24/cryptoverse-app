2b27546914a5fbc978fa05b1b6c6a13f80a96afc
Shaunak <shaunak241997@gmail.com>
2023-02-06 22:51:52 +0530

    Redux setup

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,17 @@
+// const options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/coins',
+//   params: {
+//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//     timePeriod: '24h',
+//     'tiers[0]': '1',
+//     orderBy: 'marketCap',
+//     orderDirection: 'desc',
+//     limit: '50',
+//     offset: '0',
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': '33812d6adcmsh58614cb6238f861p1be673jsn26e9a6872aeb',
+//     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+//   },
+// };
