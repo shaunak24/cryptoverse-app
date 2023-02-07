a56da2ec58324f425c126624d7697171330ee268
Shaunak <shaunak241997@gmail.com>
2023-02-07 22:38:04 +0530

    Implement Cryptocurrencies component

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Card, Col, Row, Input } from 'antd';
+import millify from 'millify';
+import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Link } from 'react-router-dom';
 
-const Cryptocurrencies = () => {
-  return <div>Cryptocurrencies</div>;
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState(cryptoList?.data?.coins);
+
+  if (isFetching) return 'Loading...';
+
+  return (
+    <>
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map((coin) => (
+          <Col className="crypto-card" xs={24} sm={12} lg={6} key={coin.uuid}>
+            <Link to={`/crypto/${coin.uuid}`}>
+              <Card
+                title={`${coin.rank}. ${coin.name}`}
+                extra={<img src={coin.iconUrl} className="crypto-image" />}
+                hoverable
+              >
+                <p>Price: {millify(coin.price)}</p>
+                <p>Market Cap: {millify(coin.marketCap)}</p>
+                <p>Daily Change: {millify(coin.change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default Cryptocurrencies;
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,10 +2,12 @@ import React from 'react';
 import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components';
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
   const { Title } = Typography;
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
 
   if (isFetching) return 'Loading...';
   const {
@@ -38,6 +40,25 @@ const Homepage = () => {
           <Statistic title="Total Markets" value={millify(totalMarkets)} />
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/news">Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 };
